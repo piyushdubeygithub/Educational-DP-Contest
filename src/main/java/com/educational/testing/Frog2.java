1188c8c086c9369cd203e4d86bbaa829b767f783
piyushdubey925 <piyush.dubey@adda247.com>
2020-11-16 03:50:30 +0000

    Frog2

--- src/main/java/com/educational/testing/Frog2.java
+++ src/main/java/com/educational/testing/Frog2.java
@@ -2,20 +2,29 @@ package com.educational.testing;
 
 import java.util.Scanner;
 
-public class Frog1 {
+public class Frog2 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int k = sc.nextInt();
         int[] h = new int[n];
         for(int i=0;i<n;i++){
             h[i] = sc.nextInt();
         }
         int[] dp = new int[n];
         dp[0] = 0;
-        dp[1] = Math.abs(h[1]-h[0]);
-        for(int i=2;i<n;i++){
-            dp[i] = Math.min(dp[i-1]+Math.abs(h[i]-h[i-1]),dp[i-2]+Math.abs(h[i] - h[i-2]));
+        for(int i=1;i<n;i++){
+            dp[i] = Integer.MAX_VALUE;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=k;j++){
+           if(i-j>=0){
+               dp[i] = Math.min(dp[i], dp[i-j] + Math.abs(h[i] - h[i-j]));
+           }else{
+               break;
+           }
+         }
         }
         System.out.println(dp[n-1]);
     }
