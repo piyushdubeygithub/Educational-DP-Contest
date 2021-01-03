e9a8b4b3cd115f17a0c48ee4add1945bee7bd666
piyushdubey925 <piyush.dubey@adda247.com>
2021-01-03 10:31:04 +0000

    reverse array

---
+++ src/main/java/dsalgo/ReverseArray.java
@@ -0,0 +1,18 @@
+package dsalgo;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] a = new int[]{1,2,3,7,11,4,89};
+        int start = 0, end = a.length-1;
+        while(start<end){
+            int temp = a[start];
+            a[start] = a[end];
+            a[end] = temp;
+            start++;
+            end--;
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
