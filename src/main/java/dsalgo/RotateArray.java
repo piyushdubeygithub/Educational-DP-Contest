eaf509bdf8b2963dc851059a10e692fc275116a3
piyushdubey925 <piyush.dubey@adda247.com>
2021-01-04 01:34:14 +0000

    cyclically-rotate-an-array-by-one

---
+++ src/main/java/dsalgo/RotateArray.java
@@ -0,0 +1,24 @@
+package dsalgo;
+
+import java.util.Arrays;
+
+public class RotateArray {
+    static int arr[] = new int[]{1, 2, 3, 4, 5};
+
+    public static void main(String[] args) {
+        System.out.println("Given Array is");
+        System.out.println(Arrays.toString(arr));
+        rotate();
+        System.out.println("Rotated Array is");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void rotate() {
+        int n = arr.length;
+        int x = arr[n-1];
+        for(int i=n-1;i>0;i--){
+            arr[i] = arr[i-1];
+        }
+        arr[0] = x;
+    }
+}
