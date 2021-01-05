61a778fdd1c0c4b2743c706a669e67d112d63813
piyushdubey925 <piyush.dubey@adda247.com>
2021-01-05 13:26:53 +0000

    largest sum contiguous subarray with all negatives

---
+++ src/main/java/dsalgo/KadaneWithAllNegativeElements.java
@@ -0,0 +1,21 @@
+package dsalgo;
+
+public class KadaneWithAllNegativeElements {
+    public static void main(String[] args) {
+        int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        int n = a.length;
+        int max_sum = maxSubArraySum(a, n);
+        System.out.println("Maximum contiguous sum is "
+                + max_sum);
+    }
+
+    private static int maxSubArraySum(int[] a, int n) {
+        int max_so_far = a[0];
+        int max_ending_here = a[0];
+        for(int i=1;i<n;i++){
+            max_ending_here = Math.max(a[i], max_ending_here+a[i]);
+            max_so_far = Math.max(max_so_far, max_ending_here);
+        }
+        return max_so_far;
+    }
+}
