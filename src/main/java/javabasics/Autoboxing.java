ca458133aba070c994a4a3a696dace31da523181
piyushdubey925 <piyush.dubey@adda247.com>
2021-01-23 11:03:22 +0000

    autoboxing unboxing

---
+++ src/main/java/javabasics/Autoboxing.java
@@ -0,0 +1,13 @@
+package javabasics;
+
+/*
+* class to demonstrate autoboxing and unboxing
+* refer: https://docs.oracle.com/javase/tutorial/java/data/autoboxing.html
+* */
+public class Autoboxing {
+    public static void main(String[] args) {
+        int i ;
+        Integer j = null;
+        i=j;//runtime error
+    }
+}
