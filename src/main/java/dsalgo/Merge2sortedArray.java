ee2c1780c9bc91c3cbfb0437d2bf03f1ad925f7a
piyushdubey925 <piyush.dubey@adda247.com>
2021-01-30 14:04:16 +0000

    merge sorted array efficient solution

--- src/main/java/dsalgo/Merge2sortedArray.java
+++ src/main/java/dsalgo/Merge2sortedArray.java
@@ -4,7 +4,7 @@ public class Merge2sortedArray {
     public static void main(String[] args) {
         int[] arr1 = {1, 3, 5, 7};
         int[] arr2 = {0, 2, 6, 8, 9};
-        merge(arr1, arr2, arr1.length, arr2.length);
+        merge1(arr1, arr2, arr1.length, arr2.length);
         for(int i=0;i<arr1.length;i++){
             System.out.print(arr1[i]+" ");
         }
@@ -13,6 +13,54 @@ public class Merge2sortedArray {
         }
     }
 
+    /*
+    * time complexity: O(m+n)
+    *
+    * */
+    public static void merge1(int arr1[], int arr2[], int m, int n) {
+        int gap = (m+n+1)/2;
+        while(gap>0){
+            for(int i=0;i<arr1.length;i++){
+                System.out.print(arr1[i]+" ");
+            }
+            for(int i=0;i<arr2.length;i++){
+                System.out.print(arr2[i]+" ");
+            }
+            System.out.println();
+            for(int i=0;i<(m+n-gap);i++){
+                if((i<arr1.length) && ((i+gap)<arr1.length)){
+                    if(arr1[i]>arr1[i+gap]){
+                        int temp = arr1[i];
+                        arr1[i] = arr1[i+gap];
+                        arr1[i+gap] = temp;
+                    }
+                }
+                else if((i>=arr1.length) && ((i+gap-arr1.length)<arr2.length)){
+                    if(arr2[i-arr1.length]>arr2[i+gap-arr1.length]){
+                        int temp = arr2[i-arr1.length];
+                        arr2[i-arr1.length] = arr2[i+gap-arr1.length];
+                        arr2[i+gap-arr1.length] = temp;
+                    }
+                }
+                else if((i<arr1.length) && ((i+gap-arr1.length)<arr2.length)){
+                    if(arr1[i]>arr2[i+gap-arr1.length]){
+                        int temp = arr1[i];
+                        arr1[i] = arr2[i+gap-arr1.length];
+                        arr2[i+gap-arr1.length] = temp;
+                    }
+                }
+            }
+            if(gap !=1){
+                gap = (gap+1)/2;
+            }else{
+                gap = gap/2;
+            }
+        }
+    }
+
+    /*
+    * time complexity O(m*n)
+    * */
     public static void merge(int arr1[], int arr2[], int n, int m) {
         // code here
         int i=n-1;
